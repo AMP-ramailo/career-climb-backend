bd5e8f69dea631359b20c7955472c18bcc8ce8c4
aashish-ramailo <aashish@ramailo.tech>
2023-10-01 02:54:32 +0545

    response type of me api

--- src/user/use.controller.ts
+++ src/user/use.controller.ts
@@ -1,9 +1,10 @@
 // auth.controller.ts
-import { Controller, Post,Get, Body,Request, NotFoundException, UseGuards  } from '@nestjs/common';
+import { Controller, Post,Get, Body,Request, NotFoundException, UseGuards, UnauthorizedException  } from '@nestjs/common';
 import { JwtAuthGuard } from './jwt-auth.guard';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiResponseProperty, ApiTags } from '@nestjs/swagger';
 import { UsersService } from './user.service';
 import { UpdateUserRoleDto } from './role.dto';
+import { User } from './user.entity';
 @ApiTags('user')
 @Controller('user')
 export class UserController {
@@ -26,10 +27,14 @@ export class UserController {
 
     return { message: 'your role updated successfully', user: user };
   }
+  @ApiOkResponse({ type: User, isArray: false })
   @Get('me')
   @UseGuards(JwtAuthGuard)
   async self(@Request() req) {
-    return { user:  req.user };
+    const user = await this.userSevices.findById(req.user.id);
+    if (!user) {
+      throw new UnauthorizedException("User not found");
+    }
+    return   user ;
   }
-
 }
