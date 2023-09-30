a7fae5fbd8243c498080b9818f6a3a9cea3452df
Manjish7 <manjish@ramailo.tech>
2023-09-30 23:31:01 +0545

    feat:added Apiproperty in dto

--- src/applicant/dto/create-applicant.dto.ts
+++ src/applicant/dto/create-applicant.dto.ts
@@ -1,12 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateApplicantDto {
+  @ApiProperty()
   experience?: number;
+  @ApiProperty()
   interview_count?: number;
+  @ApiProperty()
   dob?: Date;
+  @ApiProperty()
   address?: string;
+  @ApiProperty()
   phone?: string;
+  @ApiProperty()
   current_company?: string;
+  @ApiProperty()
   linkedin_url?: string;
+  @ApiProperty()
   github_url?: string;
+  @ApiProperty()
   rating?: number;
+  @ApiProperty()
   user_id?: number;
 }
--- src/schedule/dto/create-schedule.dto.ts
+++ src/schedule/dto/create-schedule.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateScheduleDto {
+  @ApiProperty()
   availability_date: Date;
+  @ApiProperty()
   availability_time: number;
+  @ApiProperty()
   status?: boolean;
+  @ApiProperty()
   interviewer_id?: number;
 }
