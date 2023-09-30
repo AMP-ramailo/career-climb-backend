9c649c7ef3bdb5103c9a66c91ca74f9bf61d370f
aashish-ramailo <aashish@ramailo.tech>
2023-10-01 02:16:33 +0545

    conflict resolve

--- src/user/auth/auth.services.ts
+++ src/user/auth/auth.services.ts
@@ -32,46 +32,42 @@ export class AuthService {
   }
 
 
-  async login(req, rsp):Promise<any> {
+  async login(req, rsp): Promise<any> {
     try {
-    const userData = await this.usersService.findByEmail(req.user.email)
-      if (userData)
-      { 
+      const userData = await this.usersService.findByEmail(req.user.email);
+      if (userData) {
         const payload = { sub: userData.id };
         var accessToken = this.jwtService.sign(payload);
-        console.log({ access_token:accessToken});
-       rsp.redirect(`http://localhost:3000/auth/google/success/user?access_token?${accessToken}`);
-        return {user:userData, access_token:accessToken};
-     
-       
-       
-      }else{
-        console.log("here");
+        console.log({ access_token: accessToken });
+        rsp.redirect(
+          `http://localhost:3000/auth/google/success?access_token=${accessToken}`,
+        );
+        return { user: userData, access_token: accessToken };
+      } else {
+        console.log('here');
         const user: User = new User();
         user.email = req.user.email;
-        user.name = req.user.firstName + " " + req.user.lastName ;
+        user.name = req.user.firstName + ' ' + req.user.lastName;
         user.image_url = req.user.picture;
         user.role = UserType.INTERVIEWER;
-        console.log(user)
-        const userDbResponse = await this.usersService.createUser(user.dataValues);
+        console.log(user);
+        const userDbResponse = await this.usersService.createUser(
+          user.dataValues,
+        );
 
-          const payload = { sub: userDbResponse.id };
-          var accessToken = this.jwtService.sign(payload);
-      
-          rsp.redirect(`http://localhost:3000/auth/google/success/access_token?${accessToken}`);
-  
-         return {user:userDbResponse, access_token:accessToken};
-       
+        const payload = { sub: userDbResponse.id };
+        var accessToken = this.jwtService.sign(payload);
+
+        rsp.redirect(
+          `http://localhost:3000/auth/google/success?access_token=${accessToken}`,
+        );
+
+        return { user: userDbResponse, access_token: accessToken };
       }
- 
-   
-   
-  } catch (error) {
-    // Handle any errors that occur during the process
-    console.error(error);
-    throw new Error('Login failed'); // You can customize the error handling as needed
-  }
-    
-
+    } catch (error) {
+      // Handle any errors that occur during the process
+      console.error(error);
+      throw new Error('Login failed'); // You can customize the error handling as needed
+    }
   }
 }
